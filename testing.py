51660e2cae1b393b8751b995adf062a44b5ccafa
Franco Silva <francosilva@francos-imac.lan>
2022-06-02 16:26:33 -0500

    first commit

---
+++ testing.py
@@ -0,0 +1,22 @@
+# message = "Hello World"; 
+# print(message); 
+
+# num1 = 2; 
+# num2 = 3; 
+
+# add = num1 + num2; 
+
+# print(add); 
+
+# num3 = 4; 
+# num4 = 5; 
+
+# multi = num3 * num4; 
+
+# print(multi)
+
+name = input("Enter your name: "); 
+age = input("Enter your age: "); 
+
+print("Hello " + name + "! Your " + age); 
+
